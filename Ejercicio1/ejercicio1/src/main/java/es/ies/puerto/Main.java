81063d95e3a1d9bf9b8cc758e8e15f059278b58c
Shbarroso <samuelaurix2@gmail.com>
2024-09-17 15:55:22 +0100

    Se sube ejercicio1

---
+++ Ejercicio1/ejercicio1/src/main/java/es/ies/puerto/Main.java
@@ -0,0 +1,20 @@
+package es.ies.puerto;
+
+public class Main {
+    public static void main(String[] args) {
+        int suma = 2 + 3;
+        int resta = 2 - 3;
+        int result = (4 + 8) * 3 % 5;
+        System.out.println("Hola Mundo");
+        System.out.println("Resultado de la suma: " +suma);
+        System.out.println("La resta es: " +result);
+        int valor = sumar();
+    }
+     /**
+      yo soy un metodo sumar
+      @return suma de valores
+      **/
+      static public int sumar() {
+        return  0;
+    }
+}
