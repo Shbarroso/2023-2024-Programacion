ad2ad38c5edd7fca6c1c243faf6a3f562ba0af89
Shbarroso <samuelaurix2@gmail.com>
2024-09-18 16:07:17 +0100

    Se sube ejercicio12

--- tarea1/src/main/java/es/ies/puerto/Ejercicio12.java
+++ tarea1/src/main/java/es/ies/puerto/Ejercicio12.java
@@ -1,10 +1,12 @@
 package es.ies.puerto;
 /**
- * Declara una variable de tipo short para almacenar la cantidad de días en un año (365). Asigna el valor y muéstralo por pantalla.
+ * Declara una variable de tipo short para almacenar la cantidad de días en un año (365).
+ * Asigna el valor y muéstralo por pantalla.
  * @author Shbarroso
  */
 public class Ejercicio12 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        shor diasAno = 365;
+        System.out.println("Los dias en un ano son: "+diasAno);
     }
 }
