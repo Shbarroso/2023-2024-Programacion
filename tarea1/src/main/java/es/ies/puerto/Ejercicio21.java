7aee5bce56b3f38cd2b3391ba5b3af469d31e618
Shbarroso <samuelaurix2@gmail.com>
2024-09-19 16:19:14 +0100

    Se sube ejercicio 21

---
+++ tarea1/src/main/java/es/ies/puerto/Ejercicio21.java
@@ -0,0 +1,11 @@
+package es.ies.puerto;
+public class Ejercicio21 {
+    
+    public static void main(String[] args) {
+        int edad = 18;
+        String nombre = "Dios";
+        byte numeroAlumnos = 24;
+        System.out.printf("Mi edad es %d y mi nombre es %s, tengo %d de alumnos", edad,nombre,numeroAlumnos);
+    }
+    
+}
