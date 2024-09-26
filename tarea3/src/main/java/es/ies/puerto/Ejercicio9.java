d9fc8f44b603873fdd5b875de960c7fbded634e4
Shbarroso <samuelaurix2@gmail.com>
2024-09-26 16:48:58 +0100

    se sube ejercicio9

--- tarea3/src/main/java/es/ies/puerto/Ejercicio9.java
+++ tarea3/src/main/java/es/ies/puerto/Ejercicio9.java
@@ -1,11 +1,28 @@
 package es.ies.puerto;
+
+import java.util.Scanner;
+
 /**
+ * Para hacer la fusión, los guerreros deben tener la misma cantidad de ki. Si los valores de ki no coincidente, la fusión falla.
+ * Escribe un programa que reciba los valores de ki de dos guerreros y determine si la fusión es exitosa o falla.
  * @author Shbarroso
  */
 
 public class Ejercicio9 {
+    static Scanner scanner;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        scanner = new Scanner(System.in);
+
+        System.out.println("Introducw los valores de ki1 y ki2: ");
+        int ki1 = scanner.nextInt();
+        int ki2 = scanner.nextInt();
+
+        if (ki1 == ki2) {
+            System.out.println("La fusion a tenido exito");
+        } else{
+            System.out.println("La fusion no a tenido exito");
+        }
+        scanner.close();
     }
 
 }
