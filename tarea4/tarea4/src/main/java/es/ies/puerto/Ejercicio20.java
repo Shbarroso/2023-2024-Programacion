b97577d8178151685cb7881cd824d1e4227c6415
Shbarroso <samuelaurix2@gmail.com>
2024-10-07 18:14:25 +0100

    Se sube el ejercicio 20

--- tarea4/tarea4/src/main/java/es/ies/puerto/Ejercicio20.java
+++ tarea4/tarea4/src/main/java/es/ies/puerto/Ejercicio20.java
@@ -8,5 +8,22 @@ public class Ejercicio20 {
     public static void main(String[] args) {
         String[] gemas = {"Poder", "Tiempo", "Espacio", "Tiempo", "Poder", "Poder"};
 
+        int Poder = 0;
+        int Tiempo = 0;
+        int Espacio = 0;
+
+        for (String gema : gemas) {
+            if (gema.equals("Poder")) {
+                Poder++;
+            } else if (gema.equals("Tiempo")) {
+                Tiempo++;
+            } else if (gema.equals("Espacio")) {
+                Espacio++;
+            }
+        }
+
+        System.out.println("Poder = " + Poder);
+        System.out.println("Plata = " + Tiempo);
+        System.out.println("Bronce = " + Espacio);
     }
 }
