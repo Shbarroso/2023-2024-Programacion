34d6c1af73ec06f26a820581aa9b5ebca2c04ebc
Shbarroso <samuelaurix2@gmail.com>
2024-10-20 17:05:32 +0100

    Se modifican algunos ejerciciios

--- tareaclases/tarea2/src/main/java/es/ies/puerto/Ejercicio1.java
+++ tareaclases/tarea2/src/main/java/es/ies/puerto/Ejercicio1.java
@@ -1,9 +1,7 @@
 package es.ies.puerto;
 
 public class Ejercicio1 {
-
     static private Persona persona;
-
     public static void main(String[] args) {
         persona = new Persona("juan", 30);
         persona.mostarInfo();
