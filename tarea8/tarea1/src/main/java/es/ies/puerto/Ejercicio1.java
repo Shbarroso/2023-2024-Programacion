5f4078314d3ad76deaf2d0fc9e1dfd1f59e42006
Shbarroso <samuelaurix2@gmail.com>
2024-11-29 20:12:07 +0000

    sE SUBE TAREA8

---
+++ tarea8/tarea1/src/main/java/es/ies/puerto/Ejercicio1.java
@@ -0,0 +1,10 @@
+package es.ies.puerto;
+
+/**
+ * @author Shbarroso
+ * @version 1.0.0
+ */
+public class Ejercicio1 {
+public static void main(String[] args) {
+            }
+}
---
+++ tarea8/tarea1/src/main/java/es/ies/puerto/Ejercicio19.java
@@ -0,0 +1,11 @@
+package es.ies.puerto;
+
+
+/**
+ * @author Shbarroso
+ * @version 1.0.0
+ */
+public class Ejercicio19 {
+public static void main(String[] args) {
+            }
+}
