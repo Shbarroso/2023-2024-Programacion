7bc916a43463dc7874ee3063345ebecade88d76e
Shbarroso <samuelaurix2@gmail.com>
2024-12-02 11:31:24 +0000

    Se sube primer ejercicio de la tarea 8

--- tarea8/tarea1/src/main/java/es/ies/puerto/Ejercicio1.java
+++ tarea8/tarea1/src/main/java/es/ies/puerto/Ejercicio1.java
@@ -1,10 +1,28 @@
 package es.ies.puerto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Shbarroso
  * @version 1.0.0
  */
 public class Ejercicio1 {
-public static void main(String[] args) {
-            }
+    public static void crearLista(List<Integer> list) {
+       if (list.isEmpty()){
+           for (int i = 1; i <= 10; i++){
+               list.add(i);
+           }
+       }
+    }
+    public static void eliminarLista(List<Integer> list){
+        list.removeIf(numero -> numero < 5);
+    }
+    public static void main(String[] args) {
+        List<Integer> lista1 = new ArrayList<>();
+        crearLista(lista1);
+        System.out.println(lista1);
+        eliminarLista(lista1);
+        System.out.println(lista1);
+    }
 }
