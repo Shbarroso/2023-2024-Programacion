5d4a12397d53fbb17c715cdc81f6e05583000aae
Shbarroso <samuelaurix2@gmail.com>
2024-12-03 13:05:12 +0000

    Se sube ejercicios de la tarea 8

--- tarea8/tarea1/src/main/java/es/ies/puerto/Ejercicio4.java
+++ tarea8/tarea1/src/main/java/es/ies/puerto/Ejercicio4.java
@@ -10,9 +10,22 @@ import java.util.List;
  */
 public class Ejercicio4 {
     public static void main(String[] args) {
-        List<Integer> numero = new ArrayList<>(Arrays.asList(1,2,3,4,5));
-    List<Integer> numero2 = new ArrayList<>(Arrays.asList(6,7,8,9,10));
-    numero.addAll(numero2);
+        List<Integer> lista1 = new ArrayList<>(Arrays.asList(1,2,3,4,5));
+        List<Integer> lista2 = new ArrayList<>(Arrays.asList(6,7,5,9,10));
+        System.out.println(lista1);
+        System.out.println(lista2);
+        List<Integer> lista3 = union(lista1,lista2);
+        System.out.println(lista3);
+    }
 
+    public static List<Integer> union(List<Integer> lista1, List<Integer> lista2){
+        List<Integer> union = new ArrayList<>(lista1);
+        union.addAll(lista2);
+
+        List<Integer> elementos = new ArrayList<>(lista1);
+        elementos.retainAll(lista2);
+
+        union.removeAll(elementos);
+        return union;
     }
 }
--- tarea8/tarea1/src/main/java/es/ies/puerto/Ejercicio7.java
+++ tarea8/tarea1/src/main/java/es/ies/puerto/Ejercicio7.java
@@ -1,10 +1,35 @@
 package es.ies.puerto;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Shbarroso
  * @version 1.0.0
  */
 public class Ejercicio7 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<String> palabras = new ArrayList<>();
+        palabras.add("m");
+        palabras.add("h");
+        palabras.add("h");
+        palabras.add("s");
+        palabras.add("m");
+        palabras.add("d");
+        System.out.println(palabras);
+        List<String> sinDuplicados = eliminarDuplicados(palabras);
+
+        System.out.println(sinDuplicados);
     }
+
+    public static List<String> eliminarDuplicados(List<String> lista) {
+        List<String> eliminar = new ArrayList<>();
+        for (String elemento : lista) {
+            if (!eliminar.contains(elemento)) {
+                eliminar.add(elemento);
+            }
+        }
+        return eliminar;
+    }
+
 }
