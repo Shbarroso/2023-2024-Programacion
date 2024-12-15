c117d60ec77bc4dc3169fd78df4d5f9958165930
Shbarroso <samuelaurix2@gmail.com>
2024-12-15 22:04:47 +0000

    a

--- tarea8/tarea1/src/main/java/es/ies/puerto/Ejercicio15.java
+++ tarea8/tarea1/src/main/java/es/ies/puerto/Ejercicio15.java
@@ -6,12 +6,15 @@ import java.util.List;
 /**
  * @author Shbarroso
  * @version 1.0.0
+ * Crea una lista de listas donde cada sublista representa una categoría de productos.
+ * Implementa métodos para agregar productos a una categoría, mover un producto entre categorías, y eliminar categorías vacías.
  */
 public class Ejercicio15 {
     static List<List<String>> productos = new ArrayList<>();
     static int electronica = 0;
     static int ropa = 1;
     static int libro = 2;
+    static  int alimentacion = 3;
 public static void main(String[] args) {
     productos.add(crearLista());
     productos.add(crearLista());
@@ -23,6 +26,13 @@ public static void main(String[] args) {
     addproducto(productos.get(ropa), "polo");
     addproducto(productos.get(electronica), "casco");
     System.out.println(productos);
+    moverproducto(productos.get(electronica),productos.get(ropa),"casco");
+    addproducto(productos.get(libro), "El Principito");
+    System.out.println(productos);
+    productos.add(crearLista());
+    addproducto(productos.get(alimentacion), "Papa");
+    System.out.println(productos);
+
     }
 
     public static List<String> crearLista(){
