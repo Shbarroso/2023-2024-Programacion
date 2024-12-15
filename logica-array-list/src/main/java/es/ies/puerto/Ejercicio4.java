7ac8a174ef15937455c5bbb2bb652f1c96b863d6
Shbarroso <samuelaurix2@gmail.com>
2024-12-15 13:03:19 +0000

    Se sube simulacro con el 4 y 5

--- logica-array-list/src/main/java/es/ies/puerto/Ejercicio4.java
+++ logica-array-list/src/main/java/es/ies/puerto/Ejercicio4.java
@@ -10,11 +10,14 @@ public class Ejercicio4 {
     static List<Integer> reduceList = new ArrayList<>(Arrays.asList(5, 1, 8, 3, 2));
 
   public static void main(String[] args) {
-    
+      System.out.println(reduceToSizeN(reduceList, 3));
   } 
   
   public static List<Integer> reduceToSizeN(List<Integer> list, int n) {
-
+      Collections.sort(list);
+      while (list.size() > n){
+          list.remove(0);
+      }
         return list;
     }
 
--- logica-array-list/src/main/java/es/ies/puerto/Ejercicio5.java
+++ logica-array-list/src/main/java/es/ies/puerto/Ejercicio5.java
@@ -2,17 +2,22 @@ package es.ies.puerto;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Ejercicio5 {
     static List<Integer> reverseListInput = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
 
     public static void main(String[] args) {
-        
+        System.out.println("Lista de numeros: "+reverseListInput);
+        System.out.print("Salida: "+reverseList(reverseListInput));
     }
 
     public static List<Integer> reverseList(List<Integer> list) {
-        
+        if (list == null || list.isEmpty()){
+            return list;
+        }
+        Collections.reverse(list);
         return list;
     }
 }
