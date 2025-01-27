8ac8faddabb368c56048f7feaf4e44c8ab3ee5a2
Shbarroso <samuelaurix2@gmail.com>
2025-01-27 18:44:28 +0000

    SE sube algo del ejercicio1

--- logica-fechas-2/logica-fechas/src/main/java/es/ies/puerto/Ejercicio1.java
+++ logica-fechas-2/logica-fechas/src/main/java/es/ies/puerto/Ejercicio1.java
@@ -3,7 +3,7 @@ package es.ies.puerto;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.Period;
+
 
 public class Ejercicio1 {
     
@@ -13,6 +13,8 @@ public class Ejercicio1 {
         }
         fechaInicio = LocalDateTime.of(2020, 2, 29, 23, 45, 30);
         fechaFin = LocalDateTime.of(2025, 1, 1, 8, 15, 45);
-        return fechaFin;
+        Duration diferencia =  Duration.between(fechaInicio, fechaFin);
+       
+        return null;
     }
 }
