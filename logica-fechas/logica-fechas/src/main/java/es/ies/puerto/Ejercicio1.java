58543de4c4edf58d291fbc00339cfedaccfba1c9
Shbarroso <samuelaurix2@gmail.com>
2025-02-03 21:27:02 +0000

    Se sube tarea de fichero

--- logica-fechas/logica-fechas/src/main/java/es/ies/puerto/Ejercicio1.java
+++ logica-fechas/logica-fechas/src/main/java/es/ies/puerto/Ejercicio1.java
@@ -9,6 +9,7 @@ public class Ejercicio1 {
         if(fechaStr == null || fechaStr.isEmpty()){
             return false;
         }
+
         LocalDate localDate = LocalDate.parse(fechaStr);
         return localDate.getDayOfMonth() == 13 && localDate.getDayOfWeek().equals(DayOfWeek.FRIDAY);
     }
